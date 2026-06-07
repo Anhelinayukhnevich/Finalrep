0bf00c2d3ff5184d3bac3ac10204baccef5e0a03
Anhelina Yukhnevich <anhelina.yukhnevich@dataart.com>
2026-06-07 16:47:16 +0200

    Refactor POST request: removed hardcoding, added type safety, error handling and response validation

--- src/api/task-2.ts
+++ src/api/task-2.ts
@@ -1,31 +1,55 @@
 //Junior task 2
 const baseUrl = `https://jsonplaceholder.typicode.com`
 const postsUrl = `${baseUrl}/posts`
+type PostData = {
+  title: string;
+  body: string;
+  userId: number;
+};
+//  removed hardcoded values from request body
 
-async function createPost() {
+type PostResponse = PostData & {
+  id: number;
+};
+
+async function createPost(data: PostData): Promise<PostResponse> {
+    try {
     const response = await fetch(postsUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-        title: "My first post",
-        body: "Learning API testing",
-        userId: 1
-      }),
-  })
+    body: JSON.stringify(data),
+  });
 
-  try {
-    const data = await response.json()
-    console.log(`Status: ${response.status}`)
-    console.log(`Title: ${data.title}`)
-    console.log(`Id: ${data.id}`)
-  } catch (error) {
-    console.error("Error:", error)
+  if (response.status !== 201) {
+    throw new Error(`Post was not created. Status: ${response.status}`);
   }
-  finally {
-    console.log("Post created successfully")
-  }
+  //added response status validation (201 expected for created resource)
+
+const result = (await response.json()) as PostResponse;
+
+  //console.log("Status:", response.status); 
+  //console.log(result);
+  //Old console.lo func -> removed
+
+  return result;
+} catch (error) {
+    console.error("Error:", error);
+    throw error;
+    }
+}
+async function run() {
+  const result = await createPost({
+    title: "My first post",
+    body: "Learning API testing",
+    userId: 1,
+  });
+  // removed hardcoded values from request body, so function now returns API response instead of only logging it
+
+  console.log("RESULT:", result);
 }
 
+run();
 
-createPost()
-export {}
+export {};
+
+//Also added try,catch,throw as more stable structure for testing. (  added error handling )
